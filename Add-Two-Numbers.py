ae7091dc828d9b432dbf0fc3287dec5b6b1717b8
Kirill <wmforum@bk.ru>
2023-12-18 16:43:42 +0300

    1

--- Add-Two-Numbers.py
+++ Add-Two-Numbers.py
@@ -10,13 +10,21 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        return self.list_to_int(l1) + self.list_to_int(l2)
+        return self.int_to_list(self.list_to_int(l1) + self.list_to_int(l2))
 
     def int_to_list(self, number: int) -> Optional[ListNode]:
-        result = ListNode(number % 10)
-        number//=10
+        # Создаем голову списка, которая пока пуста
+        head = ListNode()
+        current = head
+
+        # Преобразование числа в список
         while number:
-            result.next = ListNode(number%10)
+            current.next = ListNode(number % 10)
+            current = current.next
+            number //= 10
+
+        # Возвращаем список, начиная с элемента после головы
+        return head.next if head.next is not None else ListNode(0)
 
     def list_to_int(self, l: Optional[ListNode]) -> int:
         result = 0
@@ -30,6 +38,11 @@ class Solution:
 
 
 solution = Solution()
-x = ListNode(2, ListNode(4, ListNode(3)))
-y = ListNode(5, ListNode(6, ListNode(4)))
-print(solution.addTwoNumbers(x, y))
+# x = solution.int_to_list(123)
+# pass
+# x = ListNode(2, ListNode(4, ListNode(3)))
+# y = ListNode(5, ListNode(6, ListNode(4)))
+x = ListNode(0)
+y = ListNode(0)
+z = solution.addTwoNumbers(x, y)
+print(z)
