cf0b9082d695d6f4f7b9ad6b4de0dd03b1f1ef72
KirillNN <wmforum@bk.ru>
2023-12-19 23:03:38 +0300

    1

---
+++ merge-k-sorted-lists.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# https://leetcode.com/problems/merge-k-sorted-lists/
+from typing import Optional, List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        pass
+
+
+solution = Solution()
+# x = solution.int_to_list(123)
+# pass
+x1 = ListNode(1, ListNode(4, ListNode(5)))
+x2 = ListNode(1, ListNode(3, ListNode(4)))
+x3 = ListNode(2, ListNode(6))
+z = solution.mergeKLists([x1, x2, x3])
+print(z)
